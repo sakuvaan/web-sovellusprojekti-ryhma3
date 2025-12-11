e4ef477824dba92de3d89b4c53748bba6824793f
basap <kimisakaris@gmail.com>
2025-12-11 15:54:26 +0200

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,9 +7,9 @@ const JWT_SECRET = process.env.JWT_SECRET || "da53db51149c361a14745577ab67caa6";
 function sendAuthCookie(res, token) {
   res.cookie("token", token, {
     httpOnly: true,
-    secure: true,
-    sameSite: "strict",
-    maxAge: 24 * 60 * 60 * 1000
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000,
   });
 }
 
